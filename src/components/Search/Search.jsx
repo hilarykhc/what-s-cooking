f8075d40e8900819e2153fadb7da5992f0412fe0
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-04-27 13:44:40 -0400

    added styles for input form and btn

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,18 @@
 import "./Search.scss";
 
 export default function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <h2 className="search__title">Search by ingredients:</h2>
+      <form className="search__form">
+        <input
+          className="search__input"
+          type="text"
+          placeholder="Enter ingredients with a single comma in between (e.g. lettuce,egg,pepper)"
+        ></input>
+
+        <button className="search__btn">What's Cooking?</button>
+      </form>
+    </div>
+  );
 }
