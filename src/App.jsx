4d4f8844baf4398d57abe850c6fb5765bd13a457
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-24 04:13:23 -0400

    added login page jsx and scss

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Search from "./components/Search/Search";
 import Header from "./components/Header/Header";
 import Homepage from "./pages/Homepage/Homepage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import "../LoginPage/LoginPage.scss";
+
+export default function LoginPage() {
+  return (
+    <section className="login__container">
+      <h2 className="login__title">Login</h2>
+      <input className="login__input"></input>
+      <input className="login__input"></input>
+      <button className="login__button">Sign in</button>
+      <p></p>
+    </section>
+  );
+}
