ed809d438e7f33c2977e2446fa587b5636b1e72c
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-24 05:19:08 -0400

    update input placeholder icon and text

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,35 @@
 import "../LoginPage/LoginPage.scss";
+import { Link } from "react-router-dom";
+import profileIcon from "../../assets/icons/profile.svg";
 
 export default function LoginPage() {
   return (
     <section className="login__container">
       <h2 className="login__title">Login</h2>
-      <input className="login__input"></input>
-      <input className="login__input"></input>
-      <button className="login__button">Sign in</button>
-      <p></p>
+
+      <form className="login__form">
+        <input
+          className="login__input-username"
+          name="username"
+          placeholder="Enter username or email"
+        />
+        <input
+          className="login__input-password"
+          name="password"
+          placeholder="Enter password"
+          type="password"
+        />
+        <button className="login__button">Sign in</button>
+      </form>
+
+      {/* <Link> */}
+      <p className="login__signup">
+        New here?{" "}
+        <Link to="/signup">
+          <span className="login__link">Create an account</span>
+        </Link>
+      </p>
+      {/* </Link> */}
     </section>
   );
 }
