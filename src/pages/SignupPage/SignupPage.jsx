ae71843fa0418a3b937a0006beb2a5c70921e252
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-26 04:52:48 -0400

    update signup page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,5 +1,38 @@
 import "../SignupPage/SignupPage.scss";
 
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  return (
+    <>
+      <h2>Create an account.</h2>
+      <form className="login__form">
+        <p>username:</p>
+        <input
+          className="login__input-username"
+          name="username"
+          placeholder="Enter username"
+        />
+        <p>email:</p>
+        <input
+          className="login__input-password"
+          name="username"
+          placeholder="Enter email"
+        />
+        <p>password:</p>
+        <input
+          className="login__input-username"
+          name="password"
+          placeholder="Enter password"
+        />
+        <p>confirm password:</p>
+        <input
+          className="login__input-password"
+          name="password"
+          placeholder="Confirm password"
+          type="password"
+        />
+
+        <button className="login__button">Next</button>
+      </form>
+    </>
+  );
 }
