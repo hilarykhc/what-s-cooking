663460d87710a2b6e528f7ed84da90b2d8ae1455
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-26 13:12:58 -0400

    connect sign up page with server and DB

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
 import "../SignupPage/SignupPage.scss";
 
 export default function SignupPage() {
+  const [signedup, setSignedup] = useState(false);
+  const [error, setError] = useState(null);
+
   return (
     <>
       <h2 className="sign-up__title">Create an account.</h2>
-      <form className="sign-up__form">
+      <form
+        className="sign-up__form"
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          setError(null);
+          setSignedup(false);
+
+          const username = e.target.username.value;
+          const email = e.target.email.value;
+          const password = e.target.password.value;
+          const avatar = e.target.avatar.value;
+
+          if (!username || !email || !password || !avatar) {
+            alert("please fill out all fields");
+            return;
+          }
+
+          try {
+            await axios.post("http://localhost:8080/signup", {
+              username,
+              email,
+              password,
+              avatar,
+            });
+            setSignedup(true);
+          } catch (e) {
+            console.log(e);
+            setError(e?.response?.data);
+          }
+        }}
+      >
         <div className="sign-up__container">
           <p className="sign-up__text">username:</p>
           <input
@@ -25,13 +61,13 @@ export default function SignupPage() {
             placeholder="Enter password"
             type="password"
           />
-          <p className="sign-up__text">confirm password:</p>
+          {/* <p className="sign-up__text">confirm password:</p>
           <input
             className="sign-up__input sign-up__input-password"
             name="password"
             placeholder="Confirm password"
             type="password"
-          />
+          /> */}
           <p className="sign-up__text">upload an avatar:</p>
           <input
             className="sign-up__input sign-up__input-avatar"
@@ -40,6 +76,8 @@ export default function SignupPage() {
           />
 
           <button className="sign-up__button">Next</button>
+          {signedup && <div>Sign up successful, please log in</div>}
+          {error && <div>{error}</div>}
         </div>
       </form>
     </>
